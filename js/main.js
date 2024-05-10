62151dc1fecc4fae40a6855b79acdd02bf24b8e6
JaViJeC <javijec@gmail.com>
2024-05-10 16:24:59 -0300

    estilo para celular y pop up

--- js/main.js
+++ js/main.js
@@ -27,7 +27,8 @@ function mostrarCostosMano() {
 function calcular() {
   const trabajoCheckbox = document.getElementById("trabajo");
   const materialCheckbox = document.getElementById("material");
-  const costo = document.getElementById("costo");
+  const cartel = document.getElementById("cartel");
+  const fondo =document.getElementById("fondo");
   const htrabajo = document.getElementById("htrabajo").value;
   const dtrabajo = document.getElementById("dtrabajo").value;
   const mano = manoDeObra.valor;
@@ -35,7 +36,6 @@ function calcular() {
   let valormano = 0;
   const opcion =
     Number(materialCheckbox.checked) + Number(trabajoCheckbox.checked)*2;
-console.log(opcion)
   const calcularValorMaterial = () => {
     materiales.forEach((material, index) => {
       valormaterial +=
@@ -58,10 +58,10 @@ console.log(opcion)
       break;
   }
 
-  let HTML = "";
   const precio = valormano + valormaterial;
-  HTML = `Los costos son ${precio} pesos`;
-  costo.innerHTML = HTML;
+  cartel.innerHTML = `<span class="cerrar" onclick="cerrarCartel()"><img src="img/close_button.png" alt="Cerrar" class="cerrar-imagen"></span><p>Los costos son ${precio} pesos</p>`
+  cartel.style.display = "block";
+  fondo.style.display = "block";
 }
 
 //Cambiar valores de la mano de Obra
@@ -116,7 +116,6 @@ cargarValorManodeObra();
 // Clase para manejar los materiales
 class MaterialesManager {
   constructor() {
-    // Verificar si hay materiales en el localStorage
     this.materiales = this.cargarMateriales();
     this.formulario = document.getElementById("formulario-agregar");
     this.materialesLista2 = document.getElementById("materiales-lista2");
@@ -136,10 +135,11 @@ class MaterialesManager {
     let listaHTML = `<div>`;
     this.materiales.forEach((material, index) => {
       listaHTML +=
-        `<div class="row g-3 align-items-center">` +
-        `<div class="col-auto"><input type='number' id='material${index}' name='material${index}' class="form-control" value="0" placeholder="0"/></div>` +
-        `<div class="col-auto">${material.nombre} - $${material.costo}</div>` +
-        `<div class="col-auto"><button onclick="materialesManager.eliminarMaterial(${index})" class="btn btn-primary col-auto">Eliminar</button></div>` +
+        `<div class="row pw-3
+         align-items-center">` +
+        `<div class="col-4"><input type='number' id='material${index}' name='material${index}' class="form-control" value="0" placeholder="0"/></div>` +
+        `<div class="col-5">${material.nombre} - $${material.costo}</div>` +
+        `<div class="col-3"><button onclick="materialesManager.eliminarMaterial(${index})" class="btn btn-primary">Eliminar</button></div>` +
         `</div>`;
     });
     listaHTML += `</div>`;
@@ -152,14 +152,22 @@ class MaterialesManager {
     const costoInput = document.getElementById("costo-material");
     const nombre = nombreInput.value;
     const costo = parseInt(costoInput.value);
+    const cartel =document.getElementById("cartel");
+    const fondo =document.getElementById("fondo");
 
     if (this.materiales.some((material) => material.nombre === nombre)) {
-      alert(`El material '${nombre}' ya existe en la lista.`);
+      cartel.innerHTML = `<span class="cerrar" onclick="cerrarCartel()">&times;</span><p>El material '${nombre}' ya existe en la lista.</p>`
+      cartel.style.display = "block";
+      fondo.style.display = "block";
+
     } else {
       this.materiales.push({ nombre, costo });
       this.guardarMateriales();
       this.mostrarMateriales();
-      alert(`Material '${nombre}' agregado correctamente.`);
+      cartel.innerHTML = `<span class="cerrar" onclick="cerrarCartel()"><img src="img/close_button.png" alt="Cerrar" class="cerrar-imagen"></span><p>Material '${nombre}' agregado correctamente.</p>`
+      cartel.style.display = "block";
+      fondo.style.display = "block";
+
     }
 
     nombreInput.value = "";
@@ -168,11 +176,21 @@ class MaterialesManager {
 
   eliminarMaterial(index) {
     const nombre = this.materiales[index].nombre;
+    const cartel =document.getElementById("cartel");
+    const fondo =document.getElementById("fondo");
     this.materiales.splice(index, 1);
     this.guardarMateriales();
     this.mostrarMateriales();
-    alert(`Material '${nombre}' eliminado correctamente.`);
+    cartel.innerHTML = `<span class="cerrar" onclick="cerrarCartel()"><img src="img/close_button.png" alt="Cerrar" class="cerrar-imagen"></span><p>Material '${nombre}' eliminado correctamente.</p>`
+    cartel.style.display = "block";
+    fondo.style.display = "block";
   }
 }
 
 const materialesManager = new MaterialesManager();
+
+
+function cerrarCartel() {
+  document.getElementById("cartel").style.display = "none";
+  document.getElementById("fondo").style.display = "none";
+}
